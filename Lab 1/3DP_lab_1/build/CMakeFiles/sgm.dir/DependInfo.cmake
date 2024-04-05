923c0f26e04302ec3aaec9c5266db5a2d08ca984
Stefano Deriu <107172895+Steve35y@users.noreply.github.com>
2024-04-05 14:10:49 +0200

    Lab 1 Insert

---
+++ Lab 1/3DP_lab_1/build/CMakeFiles/sgm.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/stefano/Scrivania/3DP/3DP_lab_1/main.cpp" "/home/stefano/Scrivania/3DP/3DP_lab_1/build/CMakeFiles/sgm.dir/main.cpp.o"
+  "/home/stefano/Scrivania/3DP/3DP_lab_1/sgm.cpp" "/home/stefano/Scrivania/3DP/3DP_lab_1/build/CMakeFiles/sgm.dir/sgm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "/usr/include/eigen3"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab 1/3DP_lab_1/build/CMakeFiles/sgm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sgm.dir/main.cpp.o"
+  "CMakeFiles/sgm.dir/sgm.cpp.o"
+  "sgm"
+  "sgm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sgm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
